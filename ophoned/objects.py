6297a5e1176a01952a1aa8d495ef4b766a5c4ff1
Michael 'Mickey' Lauer <mickey@localhost.(none)>
2008-05-05 00:03:49 +0200

    python-ophoned: first call working in asynchronous way * add keepModemAlive to workaround OM bug #1380 * TODO: enhance pygsm to a) allow callbacks on timeout and b) get status back from activate() calls

--- ophoned/objects.py
+++ ophoned/objects.py
@@ -2,6 +2,13 @@ import config
 from config import LOG, LOG_INFO, LOG_ERR, LOG_DEBUG
 import dbus
 import dbus.service
+from modem import phoneFactory
+from gobject import timeout_add
+
+DBUS_INTERFACE_DEVICE = "org.freesmartphone.GSM.Device"
+DBUS_INTERFACE_SIM = "org.freesmartphone.GSM.Sim"
+DBUS_INTERFACE_NETWORK = "org.freesmartphone.GSM.Network"
+DBUS_INTERFACE_CALL = "org.freesmartphone.GSM.Call"
 
 class Server( dbus.service.Object ):
     DBUS_INTERFACE = "%s.%s" % ( config.DBUS_INTERFACE_PREFIX, "Server" )
@@ -21,6 +28,28 @@ class Server( dbus.service.Object ):
     def GetVersion( self ):
         return config.VERSION
 
+class AbstractAsyncResponse( object ):
+    def __init__( self, dbus_result, dbus_error ):
+        self.dbus_result = dbus_result
+        self.dbus_error = dbus_error
+        #print "(async response object %s generated)" % self
+
+    def __del__( self ):
+        pass
+        #print "(async response object %s destroyed)" % self
+
+    def __call__( self, *args, **kwargs ):
+        pass
+        #print "someone called me with args=%s, kwargs=%s" % ( repr(args), repr(kwargs) )
+        #print "i could now call", self.dbus_result, "or", self.dbus_error
+
+class AsyncResponseNone( AbstractAsyncResponse ):
+    pass
+
+class AsyncResponseBool( AbstractAsyncResponse ) :
+    def __call__( self, answer, result ):
+        self.dbus_result( result == 1 )
+
 class Device( dbus.service.Object ):
     DBUS_INTERFACE = "%s.%s" % ( config.DBUS_INTERFACE_PREFIX, "Device" )
 
@@ -30,24 +59,25 @@ class Device( dbus.service.Object ):
         dbus.service.Object.__init__( self, bus, self.path )
         LOG( LOG_INFO, "%s initialized. Serving %s at %s" % ( self.__class__.__name__, self.interface, self.path ) )
 
+        self.modem = phoneFactory( modemClass )( bus )
+        timeout_add( 6000, self.keepModemAlive )
+
+    def keepModemAlive( self ):
+        self.modem.request( "\r\nAT\r\n" )
+        return True
+
     #
     # dbus
     #
+    @dbus.service.method( DBUS_INTERFACE_DEVICE, "", "b",
+                          async_callbacks=( "dbus_ok", "dbus_error" ) )
+    def GetAntennaPower( self, dbus_ok, dbus_error ):
+        self.modem.request( '+CFUN?', AsyncResponseBool( dbus_ok, dbus_error ) )
 
-    @dbus.service.method( DBUS_INTERFACE, "", "ssss" )
-    def GetInfo( self ):
-        return self.backend.ta_request_manufacturer_identification(), \
-               self.backend.ta_request_model_identifier(), \
-               self.backend.ta_request_revision_identifier(), \
-               self.backend.ta_request_serial_number_identification()
-
-    @dbus.service.method( DBUS_INTERFACE, "", "as" )
-    def GetFeatures( self ):
-        return self.backend.ta_request_overall_capabilities()
-
-    @dbus.service.method( DBUS_INTERFACE, "", "s" )
-    def GetImei( self ):
-        return self.backend.ta_request_serial_number_identification()
+    @dbus.service.method( DBUS_INTERFACE_DEVICE, "b", "",
+                          async_callbacks=( "dbus_ok", "dbus_error" ) )
+    def SetAntennaPower( self, power, dbus_ok, dbus_error ):
+        self.modem.request( '+CFUN=%d' % 1 if power else 0, AsyncResponseNone( dbus_ok, dbus_error ), timeout = 5000 )
 
 if __name__ == "__main__":
     import dbus
