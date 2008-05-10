9610d847bf1e0d3a16310117dc5e0cb41f212c37
Jan Luebbe <jluebbe@debian.org>
2008-05-10 12:38:39 +0200

    Start oeventd by reusing ousaged

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+setup(
+    name = "PyOEventD",
+    version = "0.0.0+git",
+    author = "Jan 'Shoragan' Lübbe",
+    author_email = "jluebbe@lasnet.de",
+    url = "http://www.freesmartphone.org",
+    packages = [ "oeventd" ],
+    scripts = [ "oeventd/oeventd" ],
+    data_files = [ ("etc/dbus-1/system.d", ["etc/dbus-1/system.d/oeventd.conf"] ) ]
+)
