7714d9d337024fd1ba19339d43dac0a990ba6ea8
Michael 'Mickey' Lauer <mickey@opal.(none)>
2008-06-21 19:54:25 +0200

    first batch of repository reorganization after merging individual device daemons into frameworkd

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 setup(
-    name = "PyODeviceD",
-    version = "0.0.0+git",
-    author = "Michael 'Mickey' Lauer",
+    name = "The FreeSmartphone Framework Daemon",
+    version = "0.2.0+git",
+    author = "Michael 'Mickey' Lauer et. al.",
     author_email = "mlauer@vanille-media.de",
     url = "http://www.freesmartphone.org",
     ext_modules = [
       Extension("odeviced.modules.wireless", ["odeviced/modules/wireless.pyx"], libraries = [])
       ],
     cmdclass = {'build_ext': build_ext},
-    packages = [ "odeviced", "odeviced/modules" ],
-    scripts = [ "odeviced/odeviced" ],
-    data_files = [ ("etc/dbus-1/system.d", ["etc/dbus-1/system.d/odeviced.conf"] ) ]
+    packages = [ "frameworkd", "frameworkd/modules" ],
+    scripts = [ "frameworkd/frameworkd" ],
+    data_files = [ ("etc/dbus-1/system.d", ["etc/dbus-1/system.d/frameworkd.conf"] ) ]
 )
