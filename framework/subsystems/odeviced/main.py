ba2fa09490a16452a0a0b174459a040246a4256f
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2008-06-23 01:43:57 +0200

    odeviced/main: add GetCpuInfo method, add wildcard support to List

--- framework/subsystems/odeviced/main.py
+++ framework/subsystems/odeviced/main.py
@@ -9,25 +9,18 @@ Open Device Daemon - A plugin for the main object
 GPLv2 or later
 """
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
+import os, sys
 import dbus.service
-import os
-import sys
 from syslog import syslog, LOG_ERR, LOG_WARNING, LOG_INFO, LOG_DEBUG
 from helpers import LOG, DBUS_INTERFACE_PREFIX, DBUS_PATH_PREFIX, readFromFile, writeToFile
-from gobject import idle_add
-try:
-    import wireless
-except ImportError:
-    wireless = None
-    LOG( LOG_ERR, "wireless module not available" )
 
 #----------------------------------------------------------------------------#
 class Main( dbus.service.Object ):
 #----------------------------------------------------------------------------#
-    """An D-Bus Object implementing org.freesmartphone.Device"""
-    DBUS_INTERFACE = DBUS_INTERFACE_PREFIX  
+    """A D-Bus Object implementing org.freesmartphone.Device"""
+    DBUS_INTERFACE = DBUS_INTERFACE_PREFIX
 
     def __init__( self, bus, controller ):
         self.interface = self.DBUS_INTERFACE
@@ -40,7 +33,26 @@ class Main( dbus.service.Object ):
     #
     @dbus.service.method( DBUS_INTERFACE, "s", "ao" )
     def List( self, interface ):
-        return [x for x in self.controller.objects.values() if x.interface == interface]
+        if interface == "*":
+            return [x for x in self.controller.objects.values()]
+        elif interface.endswith( '*' ):
+            return [x for x in self.controller.objects.values() if x.interface.startswith( interface[:-1] )]
+        else:
+            return [x for x in self.controller.objects.values() if x.interface == interface]
+
+    @dbus.service.method( DBUS_INTERFACE, "", "a{sv}" )
+    def GetCpuInfo( self ):
+        cpuinfo = readFromFile( "/proc/cpuinfo" ).split( '\n' )
+        d = {}
+        for line in cpuinfo:
+            try:
+                key, value = line.split( ':' )
+            except ValueError: # no valid line
+                continue
+            d[key.strip()] = value.strip()
+        return d
+
+    # FIXME Do we want statistics (GetMemInfo, GetUptime, GetLoadAverage...)
 
 #----------------------------------------------------------------------------#
 def factory( prefix, controller ):
