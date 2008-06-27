2977a1be1b91e4efdc5226fb59aadfab053b14e6
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2008-06-27 12:12:22 +0200

    ophoned: refactor abstract call handling and pdp handling into own modem-specific module

--- framework/subsystems/ophoned/modems/abstract/mediator.py
+++ framework/subsystems/ophoned/modems/abstract/mediator.py
@@ -6,15 +6,12 @@ The Open Device Daemon - Python Implementation
 (C) 2008 Openmoko, Inc.
 GPLv2 or later
 
-Module: Mediator
-
-This module needs to be overhauled completely. Since the implementation
-of all non-trivial dbus calls need to launch subsequent AT commands
-before the final result can be delivered, we need to come up with a
-generic programmable state machine here or with some more regex's.
+Package: ophoned.modems.abstract
+Module: mediator
 
 TODO:
-
+ * refactor to using yield more often
+ * refactor to using more regexps
  * decouple from calling dbus result, we might want to reuse these functions in
    non-exported methods as well
  * recover from traceback in parsing / compiling result code
@@ -218,11 +215,9 @@ class TestMediator( AbstractMediator, AbstractYieldSupport ):
         self._commchannel = self._object.modem.communicationChannel( "TestMediator" )
         AbstractYieldSupport.__init__( self, *args, **kwargs )
 
-#=========================================================================#
-class CancelCommand( DeviceMediator ):
-#=========================================================================#
-    def trigger( self ):
-        pass
+###########################################################################
+# Device Mediators
+###########################################################################
 
 #=========================================================================#
 class DeviceGetInfo( DeviceMediator ):
@@ -292,6 +287,10 @@ class DeviceGetFeatures( DeviceMediator ):
         if "FCLASS" in response[0]:
             result["FAX"] = response[2]
         self._ok( result )
+
+###########################################################################
+# SIM Mediators
+###########################################################################
 
 #=========================================================================#
 class SimGetAuthStatus( SimMediator ):
@@ -541,6 +540,10 @@ class SimDeleteMessage( SimMediator ):
     def trigger( self ):
         self._commchannel.enqueue( "+CMGD=%d" % self.index, self.responseFromChannel, self.errorFromChannel )
 
+###########################################################################
+# Network Mediators
+###########################################################################
+
 #=========================================================================#
 class NetworkRegister( NetworkMediator ):
 #=========================================================================#
@@ -617,15 +620,11 @@ class NetworkGetCountryCode( NetworkMediator ):
     def __init__( self, dbus_object, dbus_ok, dbus_error, **kwargs ):
         dbus_error( error.UnsupportedCommand( self.__class__.__name__ ) )
 
-#
-# FIXME Here comes the call handling for the abstract modem, i.e. no additional
-# commands available to check for network status. This was the first incarnation
-# and it's broken. It should be rewritten in line with the state machine of the
-# TI Calypso call handling. Account for the missing %CPI commands by calling +CCLD
-# frequently to sync the state machine with the actual state of affairs.
-#
-# Until then I consider this code not being working at all.
-#
+###########################################################################
+# Call Mediators
+###########################################################################
+
+from .call import Call
 
 #=========================================================================#
 class CallInitiate( CallMediator ):
@@ -690,6 +689,32 @@ class CallRelease( CallMediator ):
         else:
             self._error( error.InternalException( "server does not support multiple calls yet" ) )
 
+###########################################################################
+# PDP Mediators
+###########################################################################
+
+from .pdp import Pdp
+pdpConnection = None
+
+#=========================================================================#
+class PdpActivateContext( PdpMediator ):
+#=========================================================================#
+    def trigger( self ):
+        global pdpConnection
+        if not pdpConnection:
+            pdpConnection = Pdp( self._object )
+        if pdpConnection.isActive():
+            self._ok()
+        else:
+            pdpConnection.setParameters( self.apn, self.user, self.password )
+            pdpConnection.activate()
+            self._ok()
+
+
+###########################################################################
+# Test Mediators
+###########################################################################
+
 #=========================================================================#
 class TestCommand( TestMediator ):
 #=========================================================================#
@@ -698,155 +723,6 @@ class TestCommand( TestMediator ):
     @logged
     def responseFromChannel( self, request, response ):
         self._ok( response )
-
-#=========================================================================#
-class Call( AbstractMediator ):
-#=========================================================================#
-    calls = []
-    index = 0
-    unsolicitedRegistered = False
-
-    @logged
-    def __init__( self, dbus_object, **kwargs ):
-        AbstractMediator.__init__( self, dbus_object, None, None, **kwargs )
-
-        self._callchannel = self._object.modem.communicationChannel( "CallMediator" )
-
-        self._status = "unknown"
-        self._timeout = None
-        self._index = Call.index
-        Call.index += 1
-
-        self._properties = { \
-            "type": kwargs["calltype"],
-            "ring": 0,
-            "created": time.time(),
-            "direction": kwargs["direction"],
-            "duration": 0,
-            "peer": "",
-            "reason": "" }
-
-        Call.calls.append( self )
-
-        if not Call.unsolicitedRegistered:
-            self._callchannel.setIntermediateResponseCallback( Call.intermediateResponse )
-
-    def __call__( self, dialstring ):
-        """Call (sic!)."""
-        self._callchannel.enqueue( 'D%s' % dialstring, self.responseFromChannel, self.errorFromChannel )
-        self.updateStatus( "outgoing" )
-        return self._index
-
-    def updateStatus( self, newstatus ):
-        self._status = newstatus
-        self._object.CallStatus( self._index, self._status, self._properties )
-
-    def status( self ):
-        return self._status
-
-    def klingeling( self ):
-        if self._timeout is not None:
-            gobject.source_remove( self._timeout )
-        self._timeout = gobject.timeout_add_seconds( const.TIMEOUT["RING"], self.remoteHangup )
-        self._properties["ring"] += 1
-        self.updateStatus( "incoming" )
-
-    def cancel( self ):
-        print "BEFORE CANCEL......................."
-        self._callchannel.cancelCurrentCommand()
-        print "AFTER CANCEL......................."
-        self._properties["reason"] = "cancelled"
-        self._die()
-
-    def accept( self ):
-        if self._timeout is not None:
-            gobject.source_remove( self._timeout )
-        self._callchannel.enqueue( 'A' )
-        self.updateStatus( "active" )
-
-    def remoteBusy( self ):
-        self._properties["reason"] = "remote hangup"
-        self._die()
-
-    def remoteHangup( self ):
-        self._properties["reason"] = "remote hangup"
-        self._die()
-
-    def reject( self ):
-        # TODO can/must we differenciate between hangup and reject?
-        # How do some phones send a 'BUSY' signal?
-        self.hangup()
-
-    def hangup( self ):
-        self._callchannel.enqueue( 'H' )
-        self._properties["reason"] = "local hangup"
-        self._die()
-
-    def remoteAccept( self ):
-        self._reason = ["remote accept"]
-        self.updateStatus( "active" )
-
-    def remoteClip( self, number ):
-        self._properties["peer"] = number
-        self.updateStatus( "incoming" )
-
-    def responseFromChannel( self, request, response ):
-        assert self in Call.calls, "call no longer present. must have been cancelled"
-        if response[-1] == "NO CARRIER":
-            self._properties["reason"] = "no carrier"
-            self._die()
-        elif response[-1] == "OK":
-            if self._properties["reason"] != "cancelled":
-                self.remoteAccept()
-        else:
-            print "UNKNOWN RESPONSE = ", response
-
-    def _die( self ):
-        if self._timeout is not None:
-            gobject.source_remove( self._timeout )
-        self.updateStatus( "release" )
-        Call.calls.remove( self )
-
-    @classmethod
-    @logged
-    def ring( cls, dbus_object, calltype ):
-        assert not len( cls.calls ) > 1, "ubermodem or broken code"
-        for c in cls.calls:
-            if c.status() == "incoming":
-                c.klingeling()
-                break
-        else:
-            c = Call( dbus_object, direction="incoming", calltype=calltype )
-            c.klingeling()
-
-    @classmethod
-    @logged
-    def clip( cls, dbus_object, number ):
-        # first, check the incoming calls
-        for c in cls.calls:
-            if c.status() == "incoming":
-                c.remoteClip( number )
-                break
-        else:
-            # now, check the active calls
-            for c in cls.calls:
-                if c.status() == "active":
-                    c.remoteClip( number )
-                    break
-            else:
-                raise False, "CLIP without incoming nor active call => broken code"
-
-    @classmethod
-    @logged
-    def intermediateResponse( cls, response ):
-        assert len( cls.calls ) == 1, "not handling multiple calls yet"
-        c = cls.calls[0]
-        if response == "NO CARRIER":
-            c.remoteHangup()
-        elif response == "BUSY":
-            c.remoteBusy()
-        elif response.startswith( "CONNECT" ):
-            c.talking()
 
 #=========================================================================#
 if __name__ == "__main__":
---
+++ framework/subsystems/ophoned/modems/abstract/pdp.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+"""
+The Open Device Daemon - Python Implementation
+
+(C) 2008 Michael 'Mickey' Lauer <mlauer@vanille-media.de>
+(C) 2008 Openmoko, Inc.
+GPLv2 or later
+
+Package: ophoned.modems.abstract
+Module: pdp
+
+TODO:
+    * ppp configuration data is modem specific => refactor into modem class
+
+"""
+
+from .mediator import AbstractMediator
+
+#=========================================================================#
+class Pdp( AbstractMediator ):
+#=========================================================================#
+    """
+    Encapsulates the state of (all) PDP connections on a modem
+    """
+
+    def __init__( self, dbus_object, **kwargs ):
+        AbstractMediator.__init__( self, dbus_object, None, None, **kwargs )
+        self._callchannel = self._object.modem.communicationChannel( "PdpMediator" )
+
+        self.active = False
+        self.apn = ""
+        self.user = ""
+        self.password = ""
+
+    def setParameters( self, apn, user, password ):
+        self.apn = apn
+        self.user = user
+        self.password = password
+
+    def isActive( self ):
+        return self.active
+
+    def activate( self ):
+        self.handler = GprsHandler()
+        self.handler.activate( True )
+
+    def deactivate( self ):
+        self.handler.deactivate()
+
+GPRS_APN = "internet.eplus.de"
+
+from time import time
+from os import system, kill, chmod
+from os.path import exists
+from stat import S_IRWXU, S_IRWXG, S_IRWXO
+from signal import SIGINT, signal
+from time import sleep
+from framework.config import LOG_ERR, LOG_WARNING, LOG_INFO, LOG_DEBUG, LOG
+from gobject import timeout_add, child_watch_add, spawn_async
+from syslog import openlog, LOG_PERROR, LOG_DAEMON
+import dbus
+
+# GprsHandler
+# copyright: m. dietrich
+
+#=========================================================================#
+class GprsHandler( object ):
+#=========================================================================#
+    __init_filename = '/var/tmp/gprs-connect-chat'
+    __hangup_filename = '/var/tmp/gprs-disconnect-chat'
+    __patch_files = {
+        __init_filename: r"""#!/bin/sh -e
+exec /usr/sbin/chat -v\
+    'ABORT' 'BUSY'\
+    'ABORT' 'DELAYED'\
+    'ABORT' 'ERROR'\
+    'ABORT' 'NO ANSWER'\
+    'ABORT' 'NO CARRIER'\
+    'ABORT' 'NO DIALTONE'\
+    'ABORT' 'RINGING'\
+    'ABORT' 'VOICE'\
+    'TIMEOUT' '3'\
+    '' 'ATZ'\
+    'OK-\k\k\k\d+++ATH-OK' 'ATE0'\
+    'OK' 'AT+CMEE=2'\
+    'OK' 'AT+CPIN?'\
+    'READY' '\c'\
+    'OK' 'AT+CGDCONT=1,"IP","%s"'\
+    'TIMEOUT' '180'\
+    'OK' 'ATD*99#'\
+    'CONNECT' '\d\c'
+"""% GPRS_APN,
+        '/etc/ppp/chap-secrets': '* * "%s" *\n'% '',
+        '/etc/ppp/pap-secrets': '* * "%s" *\n'% '',
+        '/etc/ppp/ip-up.d/08setupdns': """#!/bin/sh -e
+cp /var/run/ppp/resolv.conf /etc/resolv.conf
+""",
+        '/etc/ppp/ip-down.d/92removedns': """#!/bin/sh -e
+echo nameserver 127.0.0.1 > /etc/resolv.conf
+""",
+        __hangup_filename: r"""#!/bin/sh -e
+exec /usr/sbin/chat -v\
+    'ABORT' 'OK'\
+    'ABORT' 'BUSY'\
+    'ABORT' 'DELAYED'\
+    'ABORT' 'NO ANSWER'\
+    'ABORT' 'NO CARRIER'\
+    'ABORT' 'NO DIALTONE'\
+    'ABORT' 'VOICE'\
+    'ABORT' 'ERROR'\
+    'ABORT' 'RINGING'\
+    'TIMEOUT' '60'\
+    '' '\k\k\k\d+++ATH'\
+    'NO CARRIER-AT-OK' ''
+""",
+    }
+    __pppd_command = '/usr/sbin/pppd'
+    __pppd_options = [
+        '115200',
+        'nodetach',
+        'connect', __init_filename,
+        'crtscts',
+        'defaultroute',
+        'debug',
+        'disconnect', __hangup_filename,
+        'hide-password',
+        'holdoff', '3',
+        'ipcp-accept-local',
+        'ktune',
+        'lcp-echo-failure', '8',
+        'lcp-echo-interval', '3',
+        'ipcp-max-configure', '32',
+        'lock',
+        'noauth',
+        #'demand',
+        'noipdefault',
+        'novj',
+        'novjccomp',
+        #'persist',
+        'proxyarp',
+        'replacedefaultroute',
+        'usepeerdns',
+        ]
+
+    def __init__(self):
+        self.bus = dbus.SystemBus()
+        self.cpid = -1
+        self.port = ''
+        self.last_status = dict(device='')
+        timeout_add(12 * 1000, self.__poll)
+
+    def __child_watch(self, pid, condition, user_data):
+        print '__child_watch', pid, condition, user_data
+
+    def _requestChannelPath( self ):
+        """Allocate a new channel from the MUXer."""
+        oMuxer = self.bus.get_object( "org.pyneo.muxer", "/org/pyneo/Muxer" )
+        self.iMuxer = dbus.Interface( oMuxer, "org.freesmartphone.GSM.MUX" )
+        return self.iMuxer.AllocChannel( "ophoned.ppp" )
+        del self.iMuxer
+
+    def activate(self, on):
+        LOG(LOG_DEBUG, __name__, 'Activate')
+        try:
+            if on:
+                if self.cpid >= 0:
+                    raise Exception('already active')
+                self.port = str( self._requestChannelPath() )
+                if not self.port:
+                    raise Exception('no device')
+                # go direct self.port = '/dev/ttySAC0'
+                LOG(LOG_INFO, __name__, 'Activate got port', self.port)
+                for filename, content in self.__patch_files.items():
+                    LOG(LOG_DEBUG, __name__, 'change file for our needs:', filename);
+                    f = open(filename, 'w')
+                    f.write(content)
+                    f.close()
+                    chmod(filename, S_IRWXU|S_IRWXO|S_IRWXO)
+                self.cpid, _, _, _ = spawn_async(
+                    [self.__pppd_command, self.port, ] + self.__pppd_options,
+                    standard_input=False, standard_output=False, standard_error=False,
+                    #dict(PATH='/bin:/usr/bin:/sbin:/usr/sbin', ),
+                    )
+                child_watch_add(self.cpid, self.__child_watch)
+            else:
+                if self.cpid < 0: raise Exception('already inactive')
+                try:
+                    LOG(LOG_INFO, __name__, 'Activate kill pppd pid', self.cpid)
+                    kill(self.cpid, SIGINT)
+                    #LOG(LOG_INFO, __name__, 'Activate waiting for pppd')
+                    #p, r = waitpid(self.cpid, 0)
+                    #LOG(LOG_INFO, __name__, 'Activate pppd returned', r)
+                    if exists(self.port):
+                        try: open(self.port, 'rw').close()
+                        except: pass
+                finally:
+                    self.cpid = -1
+                system('ifdown usb0')
+                system('ifup usb0')
+        except Exception, e:
+            LOG(LOG_ERR, __name__, 'Activate', e)
+
+    def Status(self, newmap):
+        self.last_status = newmap
+        LOG(LOG_INFO, __name__, 'Status', self.last_status)
+
+    def GetStatus(self):
+        return self.last_status
+        LOG(LOG_INFO, __name__, 'GetStatus', self.last_status)
+
+    def get_default_route(self):
+        f = open('/proc/net/route', 'r')
+        l = f.readlines()
+        f.close()
+        for n in l:
+            n = n.split('\t')
+            if n[1] == '00000000':
+                return n[0]
+        return ''
+
+    def __poll(self):
+        status = dict(device=self.get_default_route())
+        LOG(LOG_DEBUG, __name__, '__poll', status, self.last_status)
+        if self.last_status != status:
+            self.Status(status)
+        return True
+
+#----------------------------------------------------------------------------#
+if __name__ == "__main__":
+#----------------------------------------------------------------------------#
+    import dbus
+    bus = dbus.SystemBus()
+    handler = GprsHandler( bus )
+    openlog( "gprshandler", LOG_PERROR, LOG_DAEMON )
+
+#=========================================================================#
+if __name__ == "__main__":
+#=========================================================================#
+    pass
