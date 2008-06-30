38ca32e9af74778f15a79c17c391547e6460336c
Jan Luebbe <jluebbe@debian.org>
2008-06-30 14:34:17 +0200

    Fix copy&pase error in documentation

--- framework/subsystems/ophoned/modems/ti_calypso/mediator.py
+++ framework/subsystems/ophoned/modems/ti_calypso/mediator.py
@@ -6,7 +6,7 @@ The Open Device Daemon - Python Implementation
 (C) 2008 Openmoko, Inc.
 GPLv2 or later
 
-Package: ophoned.modems.muxed4line
+Package: ophoned.modems.ti_calypso
 Module: mediator
 """
 
