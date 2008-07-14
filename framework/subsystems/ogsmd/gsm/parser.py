7c95eafb745adf315634b3b4c892eaee7dab45cd
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2008-07-14 12:43:23 +0200

    ogsmd: more tweaks to the new parser, still disabled by default though

--- framework/subsystems/ogsmd/gsm/parser.py
+++ framework/subsystems/ogsmd/gsm/parser.py
@@ -11,6 +11,8 @@ Module: parser
 
 """
 
+DEBUG = False
+
 #=========================================================================#
 class SimpleLowlevelAtParser( object ):
 #=========================================================================#
@@ -79,21 +81,21 @@ class SimpleLowlevelAtParser( object ):
 class StateBasedLowlevelAtParser( object ):
 #=========================================================================#
     """
-    A really simple lowlevel AT response parser.
+    A state machine based lowlevel AT response parser.
 
     Requirements:
     * Support feeding data from the channel in chunks of arbitrary lengths          [ok]
     * Support solicited and unsolicited responses (on the same channel)             [ok]
     * Support single (e.g. +CGMR) and multi requests (+CGMR;+CGMM;+CGMI;+CGSN)      [ok]
     * Handle one-line and multi-line responses                                      [ok]
+    * Handle multiline unsolicited responses (e.g. +CBM)                            [ok, but kind of ugly]
     * Handle multiline requests by supporting continuation '\r\n> '                 [ok, but see NOTE]
 
     Todo:
     * Detect echo mode and adjust itself (or warn)
     * Handle intermediate responses
-    * Handle multiline answers with empty lines (e.g. SMS)
-    * Handle multiline unsolicited responses (e.g. +CBM)
-    * Seamless handover to binary mode parsers
+    * Handle multiline answers with empty lines (e.g. in SMS)
+    * Seamless handover to binary mode parsers / data connections
     """
 
     def __init__( self, response, unsolicited ):
@@ -114,17 +116,22 @@ class StateBasedLowlevelAtParser( object ):
         # to support handing a haveContinuation parameter over to here.
 
         if bytes == "\r\n> ":
-            print "PARSER DEBUG: got continuation character. sending empty response"
+            if DEBUG: print "PARSER DEBUG: got continuation character. sending empty response"
             self.response( [] )
             self.state = self.reset()
             return
 
         for b in bytes:
-            print "PARSER DEBUG: [%s] feeding %s to %s" % ( ( "solicited" if haveCommand else "unsolicited" ), repr(b), self.state )
+            if DEBUG: print "PARSER DEBUG: [%s] feeding %s to %s" % ( ( "solicited" if haveCommand else "unsolicited" ), repr(b), self.state )
 
             nextstate = self.state( b, haveCommand )
             if nextstate is None:
-                print "PARSER DEBUG: WARNING: UNDEFINED PARSER STATE! trying to recover..."
+                print "PARSER DEBUG: WARNING: UNDEFINED PARSER STATE!"
+                print "previous bytes were:", repr(bytes)
+                print "current byte is:", repr(b)
+                print "lines:", repr(self.lines)
+                print "curline:", repr(curline)
+                print "solicited:", haveCommand
                 self.state = self.reset()
                 break
             else:
@@ -145,6 +152,12 @@ class StateBasedLowlevelAtParser( object ):
         else:
             if b == "\r":
                 return self.state_inline_r
+            # usually this should not happen, but some SMS are badly formatted
+            if b == '\n':
+                if s:
+                    return self.solicitedLineCompleted()
+                else:
+                    return self.unsolicitedLineCompleted()
 
     def state_inline_r( self, b, s ):
         if b == '\r':
@@ -164,16 +177,8 @@ class StateBasedLowlevelAtParser( object ):
             else:
                 return self.unsolicitedLineCompleted( True )
 
-    def state_postcommand( self, b, s ):
-        if b == '\r':
-            return self.state_postcommand_r
-
-    def state_postcommand_r( self, b, s ):
-        if b == '\n':
-            return self.reset()
-
     def solicitedLineCompleted( self, multipleR = False ):
-        print "PARSER DEBUG: solicited line completed, line=", repr(self.curline), "previous lines=", self.lines
+        if DEBUG: print "PARSER DEBUG: solicited line completed, line=", repr(self.curline), "previous lines=", self.lines
         if self.curline:
             self.lines.append( self.curline )
             # check for termination
@@ -187,23 +192,23 @@ class StateBasedLowlevelAtParser( object ):
             or self.curline.startswith( "NO ANSWER" ) \
             or self.curline.startswith( "NO CARRIER" ) \
             or self.curline.startswith( "NO DIALTONE" ):
-                print "PARSER DEBUG: solicited response completed"
+                if DEBUG: print "PARSER DEBUG: solicited response completed"
                 self.response( self.lines )
                 return self.reset()
             else:
                 self.curline = ""
                 return self.state_inline
         else:
-            print "PARSER WARNING: empty line within solicited response. Ignoring."
+            if DEBUG: print "PARSER WARNING: empty line within solicited response. Ignoring."
             self.curline = ""
             return self.state_inline
 
     def unsolicitedLineCompleted( self, multipleR = False ):
-        print "PARSER DEBUG: unsolicited line completed"
+        if DEBUG: print "PARSER DEBUG: unsolicited line completed"
         self.lines.append( self.curline )
 
         if self.hasPdu:
-            print "PARSER DEBUG: unsolicited line pdu completed, sending."
+            if DEBUG: print "PARSER DEBUG: unsolicited line pdu completed, sending."
             self.hasPdu = False
             self.unsolicited( self.lines )
             return self.reset()
@@ -213,10 +218,10 @@ class StateBasedLowlevelAtParser( object ):
         # unsolicited response. Ideally, GSM would clearly indicate whether a PDU is following
         # or not, but alas, that's not the case.
         if self.curline:
-            if self.curline.startswith( "+CBM" ) \
-            or self.curline.startswith( "+CDS" ) \
-            or self.curline.startswith( "+CMT" ):
-                print "PARSER DEBUG: message has PDU, waiting for 2nd line."
+            if self.curline.startswith( "+CBM:" ) \
+            or self.curline.startswith( "+CDS:" ) \
+            or self.curline.startswith( "+CMT:" ):
+                if DEBUG: print "PARSER DEBUG: message has PDU, waiting for 2nd line."
                 self.hasPdu = True
                 self.curline = ""
                 return self.state_inline
@@ -225,7 +230,7 @@ class StateBasedLowlevelAtParser( object ):
                 return self.reset()
 
         else:
-            print "PARSER DEBUG: unsolicited message with empty line. Ignoring."
+            if DEBUG: print "PARSER DEBUG: unsolicited message with empty line. Ignoring."
             return self.state_inline
 
 #
