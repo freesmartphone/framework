5397124644e8d90ed5d9d53643043dee58f8c0ca
Daniel Willmann <daniel@totalueberwachung.de>
2008-07-17 22:00:47 +0200

    opgsd: Support sending of variable length messages

--- framework/subsystems/ogpsd/ubx.py
+++ framework/subsystems/ogpsd/ubx.py
@@ -303,9 +303,24 @@ class UBXDevice( GPSDevice ):
             self.buffer = self.buffer[length+8:]
 
     def send( self, clid, length, payload ):
-        format = MSGFMT[(clid,length)]
+        try:
+            fmt_base = [length] + MSGFMT[(clid,length)]
+            fmt_rep = [0, "", []]
+            payload_base = payload
+        except KeyError:
+            format = MSGFMT[(clid, None)]
+            fmt_base = format[:3]
+            fmt_rep = format[3:]
+            payload_base = payload[0]
+            payload_rep = payload[1:]
+            if (length - fmt_base[0])%fmt_rep[0] != 0:
+                print "Variable length message Class", cl, "ID", id, "has wrong length", length
+                return
         stream = struct.pack("<BBBBH", SYNC1, SYNC2, CLIDPAIR[clid][0], CLIDPAIR[clid][1], length)
-        stream = stream + struct.pack(format[0], *[payload[i] for i in format[1]])
+        stream = stream + struct.pack(fmt_base[1], *[payload_base[i] for i in fmt_base[2]])
+        if fmt_rep[0] != 0:
+            for i in range(0, (length - fmt_base[0])/fmt_rep[0]):
+                stream = stream + struct.pack(fmt_rep[1], *[payload_rep[i][j] for j in fmt_rep[2]])
         stream = stream + struct.pack("<BB", *self.checksum( stream[2:] ))
         self.gpschannel.send( stream )
 
