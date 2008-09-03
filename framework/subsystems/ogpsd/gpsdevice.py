c379f2ebc1fbf310b5dff29ff083c09ec4d4624c
Daniel Willmann <daniel@totalueberwachung.de>
2008-09-03 17:03:32 +0200

    ogpsd: Hide private member variables in GPSDevice to prevent subclasses from overwriting them.

--- framework/subsystems/ogpsd/gpsdevice.py
+++ framework/subsystems/ogpsd/gpsdevice.py
@@ -23,12 +23,12 @@ class GPSDevice( dbus.service.Object ):
     """An Dbus Object implementing org.freedesktop.Gypsy"""
 
     def __init__( self, bus ):
-        self.fixstatus = 0
-        self.position = [ 0, 0, 0.0, 0.0, 0.0 ]
-        self.accuracy = [ 0, 0.0, 0.0, 0.0 ]
-        self.course = [ 0, 0, 0.0, 0.0, 0.0 ]
-        self.satellites = []
-        self.time = 0
+        self._fixstatus = 0
+        self._position = [ 0, 0, 0.0, 0.0, 0.0 ]
+        self._accuracy = [ 0, 0.0, 0.0, 0.0 ]
+        self._course = [ 0, 0, 0.0, 0.0, 0.0 ]
+        self._satellites = []
+        self._time = 0
 
         self.interface = DBUS_INTERFACE_PREFIX
         self.path = DBUS_PATH_PREFIX
@@ -37,96 +37,96 @@ class GPSDevice( dbus.service.Object ):
         logger.info("%s initialized. Serving %s at %s" % ( self.__class__.__name__, self.interface, self.path ) )
 
     def _reset( self ):
-        if self.fixstatus:
-            self.fixstatus = 0
-            self.FixStatusChanged( self.fixstatus )
-	if self.position[0]:
-            self.position[0] = 0
-            self.PositionChanged( *self.position )
-        if self.accuracy[0]:
-            self.accuracy[0] = 0
-            self.AccuracyChanged( *self.accuracy )
-        if self.course[0]:
-            self.course[0] = 0
-            self.CourseChanged( *self.course )
-        if self.satellites != []:
-            self.satellites = []
-            self.SatellitesChanged( self.satellites )
-        if self.time:
-            self.time = 0
-            self.TimeChanged( self.time )
+        if self._fixstatus:
+            self._fixstatus = 0
+            self.FixStatusChanged( self._fixstatus )
+        if self._position[0]:
+            self._position[0] = 0
+            self.PositionChanged( *self._position )
+        if self._accuracy[0]:
+            self._accuracy[0] = 0
+            self.AccuracyChanged( *self._accuracy )
+        if self._course[0]:
+            self._course[0] = 0
+            self.CourseChanged( *self._course )
+        if self._satellites != []:
+            self._satellites = []
+            self.SatellitesChanged( self._satellites )
+        if self._time:
+            self._time = 0
+            self.TimeChanged( self._time )
 
     def _updateFixStatus( self, fixstatus ):
-        if self.fixstatus != fixstatus:
-            self.fixstatus = fixstatus
-            self.FixStatusChanged( self.fixstatus )
+        if self._fixstatus != fixstatus:
+            self._fixstatus = fixstatus
+            self.FixStatusChanged( self._fixstatus )
 
     def _updatePosition( self, fields, lat, lon, alt ):
         changed = False
-        if self.position[0] != fields:
-            self.position[0] = fields
+        if self._position[0] != fields:
+            self._position[0] = fields
             changed = True
         if fields:
-            self.position[1] = self.time
-        if fields & (1 << 0) and self.position[2] != lat:
-            self.position[2] = lat
+            self._position[1] = self._time
+        if fields & (1 << 0) and self._position[2] != lat:
+            self._position[2] = lat
             changed = True
-        if fields & (1 << 1) and self.position[3] != lon:
-            self.position[3] = lon
+        if fields & (1 << 1) and self._position[3] != lon:
+            self._position[3] = lon
             changed = True
-        if fields & (1 << 2) and self.position[4] != alt:
-            self.position[4] = alt
+        if fields & (1 << 2) and self._position[4] != alt:
+            self._position[4] = alt
             changed = True
         if changed:
-            self.PositionChanged( *self.position )
+            self.PositionChanged( *self._position )
 
     def _updateAccuracy( self, fields, pdop, hdop, vdop ):
         changed = False
-        if self.accuracy[0] != fields:
-            self.accuracy[0] = fields
+        if self._accuracy[0] != fields:
+            self._accuracy[0] = fields
             changed = True
-        if fields & (1 << 0) and self.accuracy[1] != pdop:
-            self.accuracy[1] = pdop
+        if fields & (1 << 0) and self._accuracy[1] != pdop:
+            self._accuracy[1] = pdop
             changed = True
-        if fields & (1 << 1) and self.accuracy[2] != hdop:
-            self.accuracy[2] = hdop
+        if fields & (1 << 1) and self._accuracy[2] != hdop:
+            self._accuracy[2] = hdop
             changed = True
-        if fields & (1 << 2) and self.accuracy[3] != vdop:
-            self.accuracy[3] = vdop
+        if fields & (1 << 2) and self._accuracy[3] != vdop:
+            self._accuracy[3] = vdop
             changed = True
-	if changed:
-            self.AccuracyChanged( *self.accuracy )
+        if changed:
+            self.AccuracyChanged( *self._accuracy )
 
     def _updateCourse( self, fields, speed, heading, climb ):
         changed = False
-        if self.course[0] != fields:
-            self.course[0] = fields
+        if self._course[0] != fields:
+            self._course[0] = fields
             changed = True
         if fields:
-            self.course[1] = self.time
-        if fields & (1 << 0) and self.course[2] != speed:
-            self.course[2] = speed
+            self._course[1] = self._time
+        if fields & (1 << 0) and self._course[2] != speed:
+            self._course[2] = speed
             changed = True
-        if fields & (1 << 1) and self.course[3] != heading:
-            self.course[3] = heading
+        if fields & (1 << 1) and self._course[3] != heading:
+            self._course[3] = heading
             changed = True
-        if fields & (1 << 2) and self.course[4] != climb:
-            self.course[4] = climb
+        if fields & (1 << 2) and self._course[4] != climb:
+            self._course[4] = climb
             changed = True
         if changed:
-            self.CourseChanged( *self.course )
+            self.CourseChanged( *self._course )
 
     def _updateSatellites( self, satellites ):
         # Is this check sufficient or could some SVs switch channels, but
         # otherwise stay identical?
-        if self.satellites != satellites:
-            self.satellites = satellites
-            self.SatellitesChanged( self.satellites )
+        if self._satellites != satellites:
+            self._satellites = satellites
+            self.SatellitesChanged( self._satellites )
 
     def _updateTime( self, time ):
-        if self.time != time:
-            self.time = time
-            self.TimeChanged( self.time )
+        if self._time != time:
+            self._time = time
+            self.TimeChanged( self._time )
 
     # Gypsy Server interface
     # This should be implemented somewhere else once we allow different devices
@@ -155,27 +155,27 @@ class GPSDevice( dbus.service.Object ):
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Device", "", "i")
     def GetFixStatus( self ):
-        return self.fixstatus
+        return self._fixstatus
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Position", "", "iiddd" )
     def GetPosition( self ):
-        return self.position
+        return self._position
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Accuracy", "", "iddd" )
     def GetAccuracy( self ):
-        return self.accuracy
+        return self._accuracy
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Course", "", "iiddd" )
     def GetCourse( self ):
-        return self.course
+        return self._course
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Satellite", "", "a(ubuuu)" )
     def GetSatellites( self ):
-        return self.satellites
+        return self._satellites
 
     @dbus.service.method( DBUS_INTERFACE_PREFIX + ".Time", "", "i" )
     def GetTime( self ):
-        return self.time
+        return self._time
 
 
     #
