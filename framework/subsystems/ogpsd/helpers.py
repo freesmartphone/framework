44e8179a3dc2f9db54dca517b11bf440d4369231
Daniel Willmann <daniel@totalueberwachung.de>
2008-09-29 01:55:59 +0800

    ogpsd: Add file I missed in a355a0f8c6374ded40fe5446cadfb0646c3a3172

---
+++ framework/subsystems/ogpsd/helpers.py
@@ -0,0 +1,25 @@
+import logging
+logger = logging.getLogger('ogpsd')
+
+#============================================================================#
+def readFromFile( path ):
+#============================================================================#
+    try:
+        value = open( path, 'r' ).read().strip()
+    except IOError, e:
+        logger.warning( "(could not read from '%s': %s)" % ( path, e ) )
+        return "N/A"
+    else:
+        logger.debug( "(read '%s' from '%s')" % ( value, path ) )
+        return value
+
+#============================================================================#
+def writeToFile( path, value ):
+#============================================================================#
+    logger.debug( "(writing '%s' to '%s')" % ( value, path ) )
+    try:
+        f = open( path, 'w' )
+    except IOError, e:
+        logger.warning( "(could not write to '%s': %s)" % ( path, e ) )
+    else:
+        f.write( "%s\n" % value )
