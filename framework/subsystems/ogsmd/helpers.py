3b8acea059040ef8794bfbb63c1a0787e71df273
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2008-12-02 19:53:09 +0100

    ogsmd: improve killall function, patch courtesy sdhillon. closes FSO ticket #260

--- framework/subsystems/ogsmd/helpers.py
+++ framework/subsystems/ogsmd/helpers.py
@@ -111,20 +111,42 @@ class BiDict( object ):
 
 #=========================================================================#
 def processIterator():
-#=========================================================================#
-    for entry in os.listdir( "/proc" ):
-        try:
-            pid = int( entry )
-        except ValueError:
-            continue
-        else:
-            name = open( "/proc/%s/cmdline" % pid ).read().split( '\0' )[0]
-            yield name, pid
+#=========================================================================#        
+    for entry in os.listdir( "/proc" ): 
+        fileName = os.path.join( "/proc", entry, "cmdline" )
+        if os.access( fileName, os.R_OK ):
+            cmdline = file( fileName ).read()
+            executablePath = cmdline.split("\x00")[0]
+            executableName = executablePath.split(os.path.sep)[-1]
+            #entry = pid, cmdline = cmdline file contents
+            yield (entry, cmdline, executablePath, executableName) 
 
 #=========================================================================#
-def killall( nameToKill ):
+def processFinder(nameToFind, matchType):
 #=========================================================================#
-    [ os.kill( pid, signal.SIGTERM ) for name, pid in processIterator() if name == nameToKill ]
+    for entry, cmdline, executablePath, executableName in processIterator():
+        if matchType == "posix":
+            if executableName == nameToFind:
+                yield int( entry )
+        elif matchType == "weak":
+            if executablePath.find( nameToFind ) != -1:
+                yield int( entry )
+        elif matchType == "reallyweak":
+            if cmdline.find( nameToFind ) != -1:
+                yield int( entry )
+
+#=========================================================================#
+def killall( nameToKill, matchType="posix", killSignal=signal.SIGTERM ):
+#=========================================================================#
+    killedPids = []
+    for pid in processFinder( nameToKill, matchType ):
+        try:
+            os.kill( pid, killSignal )
+        except OSError: # permission denied/bad signal/etc...
+            pass
+        else:
+            killedPids.append( pid )
+    return killedPids
 
 #=========================================================================#
 if __name__ == "__main__":
