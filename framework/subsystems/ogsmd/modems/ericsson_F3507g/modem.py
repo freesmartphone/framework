b0056eadb12efdf5702e63fa0df6f1e7bd425d08
Stefan Schmidt <stefan@datenfreihafen.org>
2008-12-07 04:15:32 +0100

    ogsmd: [ericsson_F3507g] Add skeleton driver
    * Modem used on a MiniPCIe card
    * Featuring HSPA and GPS

---
+++ framework/subsystems/ogsmd/modems/ericsson_F3507g/modem.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+The Open GSM Daemon - Python Implementation
+
+(C) 2008 Michael 'Mickey' Lauer <mlauer@vanille-media.de>
+GPLv2 or later
+
+Package: ogsmd.modems.ericsson_F3507g
+Module: modem
+"""
+
+__version__ = "0.1.0"
+MODULE_NAME = "ogsmd.modems.ericsson_F3507g"
+
+import mediator
+
+from ..abstract.modem import AbstractModem
+
+from .channel import EricssonChannel
+from .unsolicited import UnsolicitedResponseDelegate
+
+from ogsmd.gsm.decor import logged
+from ogsmd.gsm.channel import AtCommandChannel
+
+#=========================================================================#
+class Sierra( AbstractModem ):
+#=========================================================================#
+
+    @logged
+    def __init__( self, *args, **kwargs ):
+        AbstractModem.__init__( self, *args, **kwargs )
+
+        # The one and only serial line
+        self._channels["SINGLE"] = EricssonChannel( self.pathfactory, "/dev/ttyACM0", modem=self )
+        # configure channels
+        self._channels["SINGLE"].setDelegate( UnsolicitedResponseDelegate( self._object, mediator ) )
+
+    def channel( self, category ):
+        return self._channels["SINGLE"]
+
+    def pathfactory( self, name ):
+        return name
+
