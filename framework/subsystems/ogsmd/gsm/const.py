08fad37bcffe5a3974301458732c728c0393506e
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2009-01-15 18:52:09 +0100

    ogsmd: const: add GSM number types seen "in the field"

--- framework/subsystems/ogsmd/gsm/const.py
+++ framework/subsystems/ogsmd/gsm/const.py
@@ -13,13 +13,16 @@ Module: const
 GSM constants, strings, formats, parse patterns, timeouts, you name it.
 """
 
+__version__ = "0.8.0"
+MODULE_NAME = "ogsmd.const"
+
 from framework import config
 from ogsmd.helpers import BiDict
 
 import re, string, os.path
 
 import logging
-logger = logging.getLogger( "ogsmd" )
+logger = logging.getLogger( MODULE_NAME )
 
 #=========================================================================#
 # format patterns
@@ -1046,9 +1049,9 @@ def phonebookTupleToNumber( nstring, ntype ):
     Returns a full number depending on a number string and a number type.
     """
 
-    # FIXME unknown types seen so far: 185, 208, 255
+    # FIXME unknown types seen so far: 160, 185, 208, 255
     # type128: network TR TCELL appears to use it as 4 digit intracompany calls
-    if ntype not in ( 129, 145 ):
+    if ntype not in ( 128, 129, 145, 160, 185, 208, 255 ):
         logger.warning( "Out-of-spec GSM number type seen: %s. Please report." % ntype )
 
     if ntype == 145: # should not include '+' then, but on some modems, it does
