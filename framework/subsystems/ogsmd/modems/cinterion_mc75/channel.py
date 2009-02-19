7252c7959acb6fdc2bd9f0074495b91834b5982b
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2009-02-19 15:21:49 +0100

    ogsmd: [CINTERION MC75] remove low level initialization; not necessary with good muxers ;)

--- framework/subsystems/ogsmd/modems/cinterion_mc75/channel.py
+++ framework/subsystems/ogsmd/modems/cinterion_mc75/channel.py
@@ -21,57 +21,7 @@ logger = logging.getLogger( "ogsmd.modems.cinterion_mc75.channel" )
 #=========================================================================#
 class CinterionModemChannel( AbstractModemChannel ):
 #=========================================================================#
-
-    def _hookLowLevelInit( self ):
-        """
-        Low level initialization of channel.
-
-        This is actually an ugly hack which is unfortunately
-        necessary since the multiplexer obviously has problems
-        wrt. to initialization (swallowing first bunch of commands now and then...)
-        To work around this, we send '\x1a\r\n' until we actually get an
-        'OK' from the modem. We try this for 5 times, then we reopen
-        the serial line. If after 10 times we still have no response,
-        we assume that the modem is broken and fail.
-        """
-        for i in itertools.count():
-            logger.debug( "(modem init... try #%d)", i+1 )
-            select.select( [], [self.serial.fd], [], 2 )
-            self.serial.write( "AT\r\n" )
-            r, w, x = select.select( [self.serial.fd], [], [], 2 )
-            if r:
-                try:
-                    buf = self.serial.inWaiting()
-                # FIXME remove catchall here
-                except:
-                    self.serial.close()
-                    path = self.pathfactory( self.name )
-                    if not path: # path is None or ""
-                        return False
-                    self.serial.port = str( path )
-                    self.serial.open()
-                    buf = self.serial.inWaiting()
-                ok = self.serial.read(buf).strip()
-                logger.debug( "read: %s", ok )
-                if "OK" in ok or "AT" in ok:
-                    break
-            logger.debug( "(modem not responding)" )
-            if i == 5:
-                logger.debug( "(reopening modem)" )
-                self.serial.close()
-                path = self.pathfactory( self.name )
-                if not path: # path is None or ""
-                    return False
-                self.serial.port = str( path )
-                self.serial.open()
-
-            if i == 10:
-                logger.warning( "(can't read from modem. giving up)" )
-                self.serial.close()
-                return False
-        logger.info( "%s: responding OK" % self )
-        self.serial.flushInput()
-        return True
+    pass
 
 #=========================================================================#
 class MiscChannel( CinterionModemChannel ):
