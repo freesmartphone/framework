bd9180c3a4639faaa5f789dc25ed0768b08533e3
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-05-02 21:55:04 +0200

    Revert "opimd: Contacts: ignore special _backend fields when merging"
    This reverts d1fa4df754461417dd2a26d134c03a1851d86f9d

--- framework/subsystems/opimd/pimd_contacts.py
+++ framework/subsystems/opimd/pimd_contacts.py
@@ -277,14 +277,13 @@ class Contact():
 
         duplicated = True
         for field_name in contact_fields:
-            if not field_name.startswith("_"):
-                try:
-                    if self.get_content()[field_name]!=contact_fields[field_name]:
-                        duplicated = False
-                        break
-                except KeyError:
+            try:
+                if self.get_content()[field_name]!=contact_fields[field_name]:
                     duplicated = False
                     break
+            except KeyError:
+                duplicated = False
+                break
 
         if duplicated:
             return True # That contacts exists, so we doesn't have to do anything to have it merged.
