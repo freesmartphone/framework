e6a560a4435e0d14b892490df5fb6ae4a10a899a
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-07-25 17:57:34 +0200

    opimd: move factory from __init__.py to opimd.py
    Signed-off-by: Sebastian Krzyszkowiak <seba.dos1@gmail.com>

--- framework/subsystems/opimd/opimd.py
+++ framework/subsystems/opimd/opimd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 #   Openmoko PIM Daemon
 #   Settings storage
@@ -5,7 +7,10 @@
 #   http://openmoko.org/
 #   http://pyneo.org/
 #
-#   Copyright (C) 2008 by Soeren Apel (abraxa@dar-clan.de)
+#   (C) 2008 Soeren Apel <abraxa@dar-clan.de>
+#   (C) 2008-2009 Openmoko, Inc.
+#   (C) 2009 Michael 'Mickey' Lauer <mlauer@vanille-media.de>
+#   (C) 2009 by Sebastian Krzyszkowiak <seba.dos1@gmail.com>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -28,3 +33,47 @@ DIN_BASE_FSO = "org.freesmartphone.PIM"
 
 DBUS_PATH_BASE = None
 DIN_BASE = None
+
+MODULE_NAME = "opimd"
+
+from opimd import *
+
+# We import the domain modules, so that there classes get registered
+import pimd_contacts
+import pimd_messages
+import pimd_calls
+# Same thing for the backend modules
+import pimb_sim_contacts_fso
+import pimb_sim_messages_fso
+import pimb_csv_contacts
+import pimb_sqlite_contacts
+import pimb_sqlite_messages
+import pimb_sqlite_calls
+import pimb_ogsmd_calls
+
+import logging
+logger = logging.getLogger( MODULE_NAME )
+
+#----------------------------------------------------------------------------#
+def factory( prefix, subsystem ):
+#----------------------------------------------------------------------------#
+    """
+    frameworkd factory method.
+    """
+    # TODO Check for exceptions
+    from domain_manager import DomainManager
+    DomainManager.init()
+
+    from backend_manager import BackendManager
+    backend_manager = BackendManager()
+
+    dbus_objects = []
+
+    # Create a list of all d-bus objects
+    for dbus_obj in DomainManager.enumerate_dbus_objects():
+        logger.debug( "adding object %s" % dbus_obj )
+        dbus_objects.append(dbus_obj)
+
+    dbus_objects.append(backend_manager)
+
+    return dbus_objects
