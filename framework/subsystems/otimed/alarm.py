721044618a57273b9bd4a9ede7321bb2fc860697
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2009-09-05 15:39:49 +0200

    otimed: alarm: catch up with API change in kernel26_rtc

--- framework/subsystems/otimed/alarm.py
+++ framework/subsystems/otimed/alarm.py
@@ -12,7 +12,7 @@ Module: alarm
 """
 
 MODULE_NAME = "otimed.alarm"
-__version__ = "0.1.4"
+__version__ = "0.1.4.1"
 
 DBUS_INTERFACE_PREFIX = "org.freesmartphone.Time.Alarm"
 DBUS_PATH_PREFIX = "/org/freesmartphone/Time/Alarm"
@@ -92,10 +92,10 @@ class AlarmController( dbus.service.Object ):
 
         # gather realtime clock dbus object
         o = bus.get_object( "org.freesmartphone.odeviced",
-                            "/org/freesmartphone/Device/RealTimeClock/0",
+                            "/org/freesmartphone/Device/RTC/0",
                             follow_name_owner_changes=True,
                             introspect=False )
-        self.rtc = dbus.Interface( o, "org.freesmartphone.Device.RealTimeClock" )
+        self.rtc = dbus.Interface( o, "org.freesmartphone.Device.RealtimeClock" )
         logger.info( "%s %s initialized. Serving %s at %s", self.__class__.__name__, __version__, self.interface, self.path )
 
     def _nameOwnerChangedHandler( self, name, old_owner, new_owner ):
