3935fdab3b5e5122d37017be47d775e3bbaf20ed
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-12-06 22:19:37 +0100

    opimd: Contacts: add ListFieldsWithType dbus method

--- framework/subsystems/opimd/pimd_contacts.py
+++ framework/subsystems/opimd/pimd_contacts.py
@@ -345,6 +345,10 @@ class ContactDomain(Domain, GenericDomain):
     def List(self):
         return self.list_fields()
 
+    @dbus_method(_DIN_FIELDS, "s", "as")
+    def ListFieldsWithType(self, type):
+        return self.list_fields_with_type(type)
+
     @dbus_method(_DIN_FIELDS, "s", "")
     def Delete(self, name):
         self.remove_field(name)
