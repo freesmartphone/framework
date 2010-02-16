b8155146fd1ec973180ca7c2d1c80c07d12a55ef
Tom Hacohen <tom@nospam>
2010-02-17 00:33:58 +0200

    Added support for handling incoming messages from ogsmd

--- framework/subsystems/opimd/pimd_messages.py
+++ framework/subsystems/opimd/pimd_messages.py
@@ -18,6 +18,13 @@ Establishes the 'messages' PIM domain and handles all related requests
 from dbus.service import FallbackObject as DBusFBObject
 from dbus.service import signal as dbus_signal
 from dbus.service import method as dbus_method
+#ogsmd interaction
+from dbus import SystemBus
+from dbus.proxies import Interface
+from dbus.exceptions import DBusException
+from functools import partial
+import dbus
+import time
 
 import re
 
@@ -30,6 +37,7 @@ from opimd import *
 
 from query_manager import QueryMatcher, SingleQueryHandler
 
+import framework.patterns.tasklet as tasklet
 from framework.config import config, busmap
 
 from pimd_generic import GenericDomain
@@ -203,6 +211,7 @@ class MessageDomain(Domain, GenericDomain):
 #----------------------------------------------------------------------------#
     name = _DOMAIN_NAME
 
+    fso_handler = None
     db_handler = None
     query_manager = None
     _dbus_path = None
@@ -224,42 +233,9 @@ class MessageDomain(Domain, GenericDomain):
         self.interface = _DIN_MESSAGES
         self.path = _DBUS_PATH_MESSAGES
 
+        self.fso_handler = MessagesFSO(self)
         self._unread_messages = 0
 
-    def register_incoming_message(self, backend, message_data, stored_on_input_backend = True):
-        #FIXME: TBD
-        logger.debug("Registering incoming message...")
-        if stored_on_input_backend:
-            message_id = self.register_message(backend, message_data)
-            self._unread_messages += 1
-            self.UnreadMessages(self._unread_messages)
-        else:
-            # FIXME: now it's just copied from Add method.
-            # Make some checking, fallbacking etc.
-
-            dbackend = BackendManager.get_default_backend(_DOMAIN_NAME)
-            result = ""
-
-            if not PIMB_CAN_ADD_ENTRY in dbackend.properties:
-            #    raise InvalidBackend( "This backend does not feature PIMB_CAN_ADD_ENTRY" )
-                 return -1
-
-            try:
-                message_id = dbackend.add_entry(message_data)
-            except AttributeError:
-            #    raise InvalidBackend( "This backend does not feature add_message" )
-                 return -1
-
-            message = self._entries[message_id]
-            result = message['Path']
-
-            # As we just added a new message, we check it against all queries to see if it matches
-            self.query_manager.check_new_entry(message_id)
-            
-        self.IncomingMessage(_DBUS_PATH_MESSAGES+ '/' + str(message_id))
-        return message_id
-
- 
     #---------------------------------------------------------------------#
     # dbus methods and signals                                            #
     #---------------------------------------------------------------------#
@@ -415,4 +391,246 @@ class MessageDomain(Domain, GenericDomain):
     @dbus_method(_DIN_FIELDS, "s", "s")
     def GetType(self, name):
         return self.field_type_from_name(name)
-               
+
+
+#----------------------------------------------------------------------------#
+class MessagesFSO(object):
+#----------------------------------------------------------------------------#
+#FIXME: Do it in a sane manner
+    name = 'FSO-Messages-Handler'
+    
+    _gsm_sim_iface = None
+    
+    _UNAVAILABLE_PART = '<???>'
+    domain = None
+#----------------------------------------------------------------------------#
+
+    def __init__(self, domain):        
+	self.domain = domain
+	
+        self.signals = False
+        self.ready_signal = False
+        self.enable()        
+
+    def __repr__(self):
+        return self.name
+
+
+    def dbus_ok(self, *args, **kargs):
+        pass
+
+    def dbus_err(self, *args, **kargs):
+        pass
+
+    def process_single_entry(self, data, incoming = True):
+        (sim_entry_id, status, number, text, props) = data
+        entry = {}
+        #FIXME: removing incoming and sanitize this function
+        
+
+        logger.debug("Processing entry \"%s\"...", text)
+
+        entry['Direction'] = 'in' if status in ('read', 'unread') else 'out'
+        
+        if status == 'read': entry['MessageRead'] = 1
+        if status == 'sent': entry['MessageSent'] = 1
+        
+        if entry['Direction'] == 'in':
+            entry['Sender'] = number
+        else:
+            entry['Recipient'] = number
+        
+        # TODO Handle text properly, i.e. make it on-demand if >1KiB
+        entry['Content'] = text
+              
+        entry['Source'] = 'SMS'
+
+        entry['SMS-combined_message'] = 0
+
+        if props.has_key('timestamp'):
+            try:
+                timestamp = props['timestamp'][:len(props['timestamp'])-6]
+                entry['Timezone'] = props['timestamp'][len(props['timestamp'])-5:]
+                entry['Timestamp'] = int(time.mktime(time.strptime(timestamp)))
+            except ValueError:
+                logger.error("Couldn't handle timestamp!")
+
+        if props.has_key('csm_seq'):
+            entry['SMS-combined_message'] = 1
+            entry['SMS-complete_message'] = 0
+            entry['SMS-csm_seq'+str(props['csm_seq'])+'_content'] = text
+
+        for field in props:
+            entry['SMS-'+field] = props[field]
+
+        if not incoming:
+            logger.debug("Message was already stored")
+            entry_id = self._domain_handlers['Messages'].register_entry(self, entry)
+            self._entry_ids.append(entry_id)
+        else:
+            logger.debug("Message is incoming!")
+            if entry['SMS-combined_message']:
+                logger.debug("It's CSM!")
+                register = 0
+                try:
+                    path = self.domain.GetSingleEntrySingleField({'Direction':'in', 'SMS-combined_message':1, 'SMS-complete_message':0, 'SMS-csm_num':entry['SMS-csm_num'], 'SMS-csm_id':entry['SMS-csm_id'], 'Source':'SMS'},'Path')
+                    if path:
+                        rel_path = path.replace('/org/freesmartphone/PIM/Messages','')
+                        result = self.domain.get_full_content(rel_path)
+                        new_content = ''
+                        complete = 1
+                        edit_data = {}
+                        for i in range(1, entry['SMS-csm_num']+1):
+                            if i==entry['SMS-csm_seq']:
+                                new_content += entry['Content']
+                                edit_data['SMS-csm_seq'+str(i)+'_content'] = entry['Content']
+                            else:
+                                try:
+                                    new_content += result['SMS-csm_seq'+str(i)+'_content']
+                                except KeyError:
+                                    new_content += self._UNAVAILABLE_PART
+                                    complete = 0
+                        if complete:
+                            edit_data['SMS-complete_message']=1
+                        edit_data['Content'] = new_content
+                        edit_data['MessageRead'] = 0
+                        #if isinstance(result['_backend_entry_id'], (list, dbus.Array)):
+                        #    result['_backend_entry_id'].append(sim_entry_id)
+                        #    edit_data['_backend_entry_id'] = result['_backend_entry_id']
+                        #else:
+                        #    edit_data['_backend_entry_id'] = [result['_backend_entry_id'], sim_entry_id]
+                        self.domain.Update(edit_data, rel_path)
+                    else:
+                        register = 1
+                        if entry['SMS-csm_seq']>1:
+                            entry['Content']=self._UNAVAILABLE_PART+entry['Content']
+                        if entry['SMS-csm_seq']<entry['SMS-csm_num']:
+                            entry['Content']=entry['Content']+self._UNAVAILABLE_PART
+                        logger.debug('CSM: first part')
+                except:
+                    register = 1
+                    logger.error('%s: failed to handle CSM message!', self.name)
+            else:
+                entry_id = self.AddIncoming(self, entry)
+                
+
+    def process_split_entries(self, entries):
+	#FIXME: remove
+        last_msg = []
+        text_msg = ''
+        ids = []
+        max_id = -1
+
+        for i in range(1, len(entries)+1):
+            added = 0
+            for msg in entries:
+                if msg[4]['csm_seq']==i:
+                    added = 1
+                    text_msg += msg[3]
+                    ids.append(msg[0])
+                    if i>max_id:
+                        max_id=i
+                        last_msg = msg
+            if not added:
+                text_msg += self._UNAVAILABLE_PART
+
+        last_msg[4]['combined_message'] = 1
+        if len(entries)==last_msg[4]['csm_num']:
+            last_msg[4]['complete_message'] = 1
+        else:
+            last_msg[4]['complete_message'] = 0
+
+        for msg in entries:
+            last_msg[4]['csm_seq'+str(msg[4]['csm_seq'])+'_content']=msg[3]
+
+        combined_msg = [ids,last_msg[1],last_msg[2],text_msg,last_msg[4]]
+        self.process_single_entry(combined_msg)
+
+    def disable(self):
+        if self.ready_signal:
+            self.readysignal.remove()
+            self.authsignal.remove()
+            self.ready_signal = False
+        if self.signals:
+            self.imsignal.remove()
+            self.ismsignal.remove()
+            self.imrsignal.remove()
+            self.signals = False
+
+    def enable(self):
+        self.bus = SystemBus()
+
+                
+        try:
+            self.gsm = self.bus.get_object('org.freesmartphone.ogsmd', '/org/freesmartphone/GSM/Device')
+            self.gsm_sim_iface = Interface(self.gsm, 'org.freesmartphone.GSM.SIM')
+            self.gsm_sms_iface = Interface(self.gsm, 'org.freesmartphone.GSM.SMS')
+            self.gsm_device_iface = Interface(self.gsm, 'org.freesmartphone.GSM.Device')
+
+            self.install_signal_handlers()
+            self._initialized = True
+        except DBusException, e:
+            logger.warning("%s: Could not request SIM messagebook from ogsmd (%s)", self.name, e)
+            logger.info("%s: Waiting for SIM being ready...", self.name)
+            if not self.ready_signal:
+                try:
+                    self.readysignal = self.bus.add_signal_receiver(self.handle_sim_ready, signal_name='ReadyStatus', dbus_interface='org.freesmartphone.GSM.SIM', bus_name='org.freesmartphone.ogsmd')
+                    self.authsignal = self.bus.add_signal_receiver(self.handle_auth_status, signal_name='AuthStatus', dbus_interface='org.freesmartphone.GSM.SIM', bus_name='org.freesmartphone.ogsmd')
+                    logger.info('%s: Signal listeners about SIM status installed', self.name)
+                    #self.gsm_sim_iface.connect_to_signal("ReadyStatus", self.handle_sim_ready)
+                    self.ready_signal = True
+                except:
+                    logger.error("%s: Could not install signal handler!", self.name)
+
+    def handle_incoming_stored_message(self, message_id):
+	logger.error("Got incoming stored message, shouldn't happen")
+	#SHOLUD WE HANDLE?
+        self.gsm_sim_iface.RetrieveMessage(
+            message_id,
+            reply_handler=partial(self.process_incoming_stored_entry, message_id=message_id),
+            error_handler=self.dbus_err
+            )
+
+    def handle_incoming_message(self, number, text, props):
+        try:
+            self.process_single_entry((-1, "unread", number, text, props), True)
+            self.gsm_sms_iface.AckMessage('', {}, reply_handler=self.dbus_ok, error_handler=self.dbus_err)
+        except:
+            self.gsm_sms_iface.NackMessage('', {}, reply_handler=self.dbus_ok, error_handler=self.dbus_err)
+            logger.error("Message nacked!")
+
+    def install_signal_handlers(self):
+        """Hooks to some d-bus signals that are of interest to us"""
+        if not self.signals:
+            try:
+                self.imsignal = self.gsm_sms_iface.connect_to_signal("IncomingMessage", self.handle_incoming_message)
+                self.ismsignal = self.gsm_sim_iface.connect_to_signal("IncomingStoredMessage", self.handle_incoming_stored_message)
+                self.imrsignal = self.gsm_sms_iface.connect_to_signal("IncomingMessageReceipt", self.handle_incoming_message_receipt)
+                logger.info("%s: Installed signal handlers", self.name)
+                self.signals = True
+                self.gsm_device_iface.SetSimBuffersSms(self.am_i_default(), reply_handler=self.dbus_ok, error_handler=self.dbus_err)
+            except:
+                logger.error("%s: Could not install signal handlers!", self.name)
+
+    def handle_auth_status(self, ready):
+        if ready=='READY':
+            self.enable()    
+
+    def handle_sim_ready(self, ready):
+        if ready:
+            self.load_entries().start()
+#FIXME: DROP?
+    def handle_incoming_message_receipt(self, number, text, props):
+        path = self.domain.GetSingleEntrySingleField({'SMS-message-reference':props['message-reference'], 'Direction':'out', 'Source':'SMS', 'SMS-status-report-request':1},'Path')
+        if path:
+            rel_path = path.replace('/org/freesmartphone/PIM/Messages','')
+            try:
+                if props['status']==0:
+                    self.domain.Update({'SMS-delivered':1, 'SMS-message-reference':''}, rel_path)
+                else:
+                    self.domain.Update({'SMS-delivered':0, 'SMS-message-reference':''}, rel_path)
+            except:
+                logger.error("%s: Could not store information about delivery report for message %s!", self.name, path)
+        else:
+            logger.info("%s: Delivery report about non-existient message!", self.name)
+
